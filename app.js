6285a25e9842f4feaac1bf32685da615d95ca2ac
Jing <new216@ymail.com>
2021-09-01 23:39:43 +0800

    很醜

---
+++ app.js
@@ -0,0 +1,22 @@
+const express=require("express");
+const app=express();
+const ejs=require("ejs");
+
+
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.set("view-engine","ejs");
+
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+})
+
+
+app.listen(3000,()=>{
+    console.log("server is runing ");
+})
